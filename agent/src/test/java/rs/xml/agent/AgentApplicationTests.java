37d317c922856772da500967072eccf91c43fdd6
Slobodan <slobodansrk@gmail.com>
2020-05-13 10:40:59 +0200

    Pocetni fajlovi za agentsku aplikaciju

---
+++ agent/src/test/java/rs/xml/agent/AgentApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.xml.agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
