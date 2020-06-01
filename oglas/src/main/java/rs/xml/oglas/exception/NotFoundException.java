6d45844680f5305c2b9645b380652fe25e683548
Slobodan <slobodansrk@gmail.com>
2020-06-01 17:48:33 +0200

    exception u oglasi, sitne izmene u OglasDTO

---
+++ oglas/src/main/java/rs/xml/oglas/exception/NotFoundException.java
@@ -0,0 +1,13 @@
+package rs.xml.oglas.exception;
+
+@SuppressWarnings("serial")
+public class NotFoundException extends RuntimeException {
+	
+	public NotFoundException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public NotFoundException(String message) {
+		super(message);
+	}
+}
