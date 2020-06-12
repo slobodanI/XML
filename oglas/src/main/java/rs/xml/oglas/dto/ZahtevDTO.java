fd4a5946022df1ea778752cfddf2b06e6963e81e
stjepanovicM <stjepanovicmilos@gmail.com>
2020-06-12 18:10:01 +0200

    Auto stash before merge of "master" and "origin/master"

--- oglas/src/main/java/rs/xml/oglas/dto/ZahtevDTO.java
+++ oglas/src/main/java/rs/xml/oglas/dto/ZahtevDTO.java
@@ -113,6 +113,14 @@ public class ZahtevDTO {
 	public void setPodnosilacUsername(String podnosilacUsername) {
 		this.podnosilacUsername = podnosilacUsername;
 	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 	
 
 }
