fbcdf4e7cce4b3197b31310d0bf6b4fdcd3cc72e
stjepanovicM <stjepanovicmilos@gmail.com>
2020-06-26 20:37:38 +0200

    Front za signup i change-password

---
+++ agent/src/main/resources/static/User/js/change-password.js
@@ -0,0 +1,79 @@
+$(document).ready(function() {	
+	
+	if(sessionStorage.getItem("token")) {
+		token = JSON.parse(sessionStorage.token);
+		whoami();
+	} else {
+		console.log("No token in session memory...");
+		window.location = "../login.html";
+	}
+	
+	$("#form-change-password").submit(function(event) {
+		event.preventDefault();
+		changePass();
+	});
+	
+});
+
+
+function changePass() {
+
+
+	var oldPassword = $('#input-oldpassword').val();
+	var newPassword = $('#input-newpassword').val();
+
+	var obj = JSON.stringify(
+			{oldPassword, newPassword}
+	);
+	
+	
+	$.post({
+		url: '/change-password',
+		data: JSON.stringify(
+				{oldPassword, newPassword}
+				),
+		headers: {
+			        'Auth': 'Bearer ' + token
+			    },
+		contentType: 'application/json',
+		success: function(){
+			alert("Uspešno ste promenili sifru");
+		},
+		error: function(message) {
+			alert("Password not correct!");
+            console.log("Error: ", message);
+        }
+	});
+}
+
+
+function whoami() {
+	if(sessionStorage.getItem("token")) {
+		token = JSON.parse(sessionStorage.token);
+	} else {
+		console.log("No token in session memory...");
+		return;
+	}
+	
+	$.get({
+		url: '/whoami',
+		headers: {
+	        'Auth': 'Bearer ' + token
+	    },
+		success: function(user) {
+			var ROLES = "";
+			for(var role of user.authorities){					
+				ROLES += role.authority+","
+			}
+			if(ROLES.includes("ROLE_USER") || ROLES.includes("ROLE_USER_LIMITED") || ROLES.includes("ROLE_AGENT") ) {
+				// 
+			} else {
+				window.location = "../login.html";
+			}
+			
+		},
+		error: function() {
+			window.location = "../login.html";
+		}
+	});
+}
